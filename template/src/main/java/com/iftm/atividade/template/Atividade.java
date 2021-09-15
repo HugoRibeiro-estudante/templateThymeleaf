247f811e738c84c409bdef9c533500db4ef616b7
Hugo Ribeiro Alves <hugo.alves@estudante.iftm.edu.br>
2021-09-15 18:52:10 -0300

    projeto final

--- template/src/main/java/com/iftm/atividade/template/Atividade.java
+++ template/src/main/java/com/iftm/atividade/template/Atividade.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Atividade{
     
-    @RequestMapping("/requisicao")
-    public String mvc(){
-        return "at1";
+    @RequestMapping("/pag1")
+    public String mvc1(){
+        return "pag1";
+    }
+
+    @RequestMapping("/pag2")
+    public String mvc2(){
+        return "pag2";
     }
 
 }
